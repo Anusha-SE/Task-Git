3400158f7f6b8c314897f3675130868df55e48fb
Anusha-SE <anushathirechanku@gmail.com>
2022-11-11 12:34:34 +0530

    First time push

---
+++ src/test/java/task/Git1.java
@@ -0,0 +1,13 @@
+package task;
+
+public class Git1 {
+	private void method1() {
+		System.out.println("Method 1");
+	}
+	private void method2() {
+		System.out.println("Method 2");
+	}
+	private void method3() {
+		System.out.println("Method 3");
+	}
+}
