96d87d7cd08454cc69a8194536a7c37bd20fc669
Anusha-SE <anushathirechanku@gmail.com>
2022-11-12 22:35:58 +0530

    updated code

--- src/test/java/task/Git1.java
+++ src/test/java/task/Git1.java
@@ -10,4 +10,22 @@ public class Git1 {
 	private void method3() {
 		System.out.println("Method 3");
 	}
+    public void method7() {
+		System.out.println("Method 7");
+	}
+    public void method8() {
+		System.out.println("Method 8");
+	}
+    public void method9() {
+		System.out.println("Method 9");
+	}
+    public void method10() {
+		System.out.println("Method 10");
+	}
+    public void method11() {
+		System.out.println("Method 11");
+	}
+    public void method112() {
+		System.out.println("Method 12");
+	}
 }
